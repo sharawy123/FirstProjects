168362dc2f7dc27f8ee2aa05ef530cabbc428270
Yousef Ahmed <Uouoking323@gmail.com>
2024-10-17 17:01:46 +0300

    UI Whole project is done

--- lib/taps/radio/radio_tab.dart
+++ lib/taps/radio/radio_tab.dart
@@ -9,7 +9,7 @@ class RadioTab extends StatelessWidget {
   Widget build(BuildContext context) {
     SettingsProvider settingsProvider = Provider.of<SettingsProvider>(context);
     return Column(
-    //  crossAxisAlignment: CrossAxisAlignment.stretch,
+      //  crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         SizedBox(height: 105),
         Image.asset('assets/images/Radiologo.png',width: 412,height: 222,fit: BoxFit.fill,),
@@ -17,16 +17,16 @@ class RadioTab extends StatelessWidget {
         Text('إذاعة القرآن الكريم',style: TextStyle(fontWeight: FontWeight.w500,fontSize: 25,color:settingsProvider.isDark ? AppTheme.whitee : AppTheme.black, ),),
         SizedBox(height: 57),
         Row(
-            children: [
-              Container(
-                  margin:EdgeInsets.only(left: 92),
-    child: Icon(Icons.skip_previous,color:settingsProvider.isDark ? AppTheme.gold : AppTheme.lightPrimary,size: 35,)),
-              Container( margin: EdgeInsets.only(left: 39),
-                  child: Icon(Icons.play_arrow,color:settingsProvider.isDark ? AppTheme.gold : AppTheme.lightPrimary,size: 50,)),
-              Container(margin: EdgeInsets.only(left: 50),
-                  child: Icon(Icons.skip_next,color:settingsProvider.isDark ? AppTheme.gold : AppTheme.lightPrimary,size: 35,)),
+          children: [
+            Container(
+                margin:EdgeInsets.only(left: 92),
+                child: Icon(Icons.skip_previous,color:settingsProvider.isDark ? AppTheme.gold : AppTheme.lightPrimary,size: 35,)),
+            Container( margin: EdgeInsets.only(left: 39),
+                child: Icon(Icons.play_arrow,color:settingsProvider.isDark ? AppTheme.gold : AppTheme.lightPrimary,size: 50,)),
+            Container(margin: EdgeInsets.only(left: 50),
+                child: Icon(Icons.skip_next,color:settingsProvider.isDark ? AppTheme.gold : AppTheme.lightPrimary,size: 35,)),
 
-            ],
+          ],
         )
       ],
     )

1d6451713c5751a1b57680f42e0c5e5e3efa2118
Yousef Ahmed <Uouoking323@gmail.com>
2024-10-17 14:54:07 +0300

    shared preferences package added

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'app_theme.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 void main(){
   runApp(ChangeNotifierProvider(
     create: (_)=> SettingsProvider(),

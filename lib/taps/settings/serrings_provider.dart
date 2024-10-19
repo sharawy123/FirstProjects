17f6648a116e06967b7875512cf2ba6ea494d7e2
Yousef Ahmed <Uouoking323@gmail.com>
2024-10-19 20:00:31 +0300

    user Theme and lang are saved

--- lib/taps/settings/serrings_provider.dart
+++ lib/taps/settings/serrings_provider.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 class SettingsProvider with ChangeNotifier{
   ThemeMode themeMode =ThemeMode.light;
   String LangCode ='en';
   bool get isDark =>themeMode ==ThemeMode.dark;
 
   String get backgroudImageName =>isDark? 'darkbg' : 'background1' ;
-  void changeTheme(ThemeMode selectedTheme)
-  {
-    themeMode=selectedTheme;
+  //late  bool themeSave;
+
+  SettingsProvider() {
+    loadData(); // Call loadData when the provider is initialized
+  }
+  void loadData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? savedIsDark = prefs.getBool('dark');
+
+    if (savedIsDark != null) {
+      themeMode = savedIsDark ? ThemeMode.dark : ThemeMode.light;
+
+      String? LangIsSaved =prefs.getString('lang');
+     LangCode=LangIsSaved!;
+
+    }
     notifyListeners();
   }
-  void changeLang(String SelectedLang)
+  void changeTheme(ThemeMode selectedTheme) async
+  {
+    themeMode=selectedTheme;
+    SharedPreferences prefs=await SharedPreferences.getInstance();
+    prefs.setBool("dark", isDark);
+    notifyListeners();
+  }
+  void changeLang(String SelectedLang)async
   {
     if(SelectedLang==LangCode)return;
     LangCode=SelectedLang;
+    SharedPreferences prefs=await SharedPreferences.getInstance();
+    prefs.setString('lang',LangCode );
     notifyListeners();
 
   }
+
+
 }
